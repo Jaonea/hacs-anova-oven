a2812523a1caa66bdc4078551c456da762b0681b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2025-03-08 06:52:25 -0700

    Update api.py

--- custom_components/anova_oven/api.py
+++ custom_components/anova_oven/api.py
@@ -146,9 +146,7 @@ class AnovaOvenApi:
                                                             ]["setpoint"].get("fahrenheit", 0),
                                                         ),
                                                         dosed=bulbs["wet"]["dosed"],
-                                                        dose_failed=bulbs["wet"][
-                                                            "doseFailed"
-                                                        ],
+                                                        dose_failed=bulbs["wet"].get("doseFailed", false),
                                                     ),
                                                     rear_heating=APOSensor.Nodes.HeatingElement(
                                                         watts=he["rear"]["watts"],
