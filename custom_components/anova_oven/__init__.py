86e091bd8cb736c34a9a328e7d9d9411600c184e
Vincenzo Chianese <vincenz.chianese@icloud.com>
2025-03-09 16:17:10 -0600

    no timer added

--- custom_components/anova_oven/__init__.py
+++ custom_components/anova_oven/__init__.py
@@ -235,7 +235,6 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             preheat_stage,
             id=f"{PLATFORM}-{uuid.uuid4()}",
             do=dataclasses.replace(preheat_stage.do,type="cook"),
-            timer_added=timer is not None,
             timer=APOStage.Timer(
                 initial=timer["hours"] * 3600 + timer["minutes"] * 60 + timer["seconds"],
                 entry={}
